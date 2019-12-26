dd72cbd999742dcb8940f68d724b65231f7ba26f
Lucía Calzado <Luca Calzado@DESKTOP-TVMBU9D>
2019-12-26 18:04:29 +0100

    Clase TarjetaDebitoDAO.java
    Clase TarjetaDebitoDAO terminada.
    Cambios: código mejorado según checkstyle, sintaxis arreglada según PMD.

--- src/main/java/edu/uclm/esi/iso2/banco20193capas/dao/TarjetaDebitoDAO.java
+++ src/main/java/edu/uclm/esi/iso2/banco20193capas/dao/TarjetaDebitoDAO.java
@@ -1,9 +1,10 @@
 package edu.uclm.esi.iso2.banco20193capas.dao;
-
 import org.springframework.data.repository.CrudRepository;
 
 import edu.uclm.esi.iso2.banco20193capas.model.TarjetaDebito;
-
+/**
+ * @author Macario
+ */
 public interface TarjetaDebitoDAO extends CrudRepository<TarjetaDebito, Long>{
 
 }
