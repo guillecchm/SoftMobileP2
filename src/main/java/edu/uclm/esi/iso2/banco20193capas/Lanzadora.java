e8e698bb01c2a43d756aa489a4438e52491c49ae
Lucía Calzado <Luca Calzado@DESKTOP-TVMBU9D>
2019-12-26 17:47:54 +0100

    Clase Lanzadora.java
    Clase Lanzadora terminada.
    Cambios: código mejorado según checkstyle, sintaxis arreglada según PMD

--- src/main/java/edu/uclm/esi/iso2/banco20193capas/Lanzadora.java
+++ src/main/java/edu/uclm/esi/iso2/banco20193capas/Lanzadora.java
@@ -1,31 +1,34 @@
 package edu.uclm.esi.iso2.banco20193capas;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import edu.uclm.esi.iso2.banco20193capas.model.Cliente;
 import edu.uclm.esi.iso2.banco20193capas.model.Cuenta;
 
+/**
+ * @author Macario
+ */
 @SpringBootApplication
-public class Lanzadora {
+public final class Lanzadora {
 
-	public static void main(String[] args) throws Exception {
+	/**
+	 * MAIN CLASS
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(final String[] args) throws Exception {
 		SpringApplication.run(Lanzadora.class, args);
-	
-		
+
 		try {
-			Cliente pepe = new Cliente("12345X", "Pepe", "Pérez");
+			final Cliente pepe = new Cliente("12345X", "Pepe", "Pérez");
 			pepe.insert();
-			
-			Cuenta cuenta = new Cuenta();
+
+			final Cuenta cuenta = new Cuenta();
 			cuenta.addTitular(pepe);
 			cuenta.insert();
-			
+
 			cuenta.ingresar(1000);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
-
 }
