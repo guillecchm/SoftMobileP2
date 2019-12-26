37bfcc9dc79d3fa906fb1089c39d5a7d00fc2743
Guillermo Carmelo Chacón Millán <guillermocarmelo.chacon@alu.uclm.es>
2019-12-26 20:30:19 +0100

    Mantenimiento Guillermo v1.0

--- src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/SaldoInsuficienteException.java
+++ src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/SaldoInsuficienteException.java
@@ -1,7 +1,16 @@
 package edu.uclm.esi.iso2.banco20193capas.exceptions;
 
+/**
+ * 
+ * @author Macario
+ *
+ */
 public class SaldoInsuficienteException extends Exception {
-	public SaldoInsuficienteException() {
-		super("Saldo insuficiente para el importe solicitado");
-	}
+    /**
+     * Excepción que se produce cuando se intenta sacar más dinero que
+     * saldo se tiene en una cuenta.
+     */
+    public SaldoInsuficienteException() {
+        super("Saldo insuficiente para el importe solicitado");
+    }
 }
