48dcb774b2bfc7149d95d70abae3f9c540e40f10
Antonio Felipe Rojo <Antonio.Felipe@alu.uclm.es>
2019-12-26 20:36:35 +0100

    Mantenimiento Antonio v1.0

--- src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/ImporteInvalidoException.java
+++ src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/ImporteInvalidoException.java
@@ -1,7 +1,16 @@
 package edu.uclm.esi.iso2.banco20193capas.exceptions;
 
+/**
+ * Clase ImporteInvalidoException.
+ * @author Macario
+ *
+ */
 public class ImporteInvalidoException extends Exception {
-	public ImporteInvalidoException(double importe) {
-		super("El importe " + importe + " no es válido para esta operación");
-	}
+    /**
+     * Imprime mensaje importe no válido.
+     * @param importe Importe de la operación.
+     */
+    public ImporteInvalidoException(final double importe) {
+        super("El importe " + importe + " no es válido para esta operación");
+    }
 }
