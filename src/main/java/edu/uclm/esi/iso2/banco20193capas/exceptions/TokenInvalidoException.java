fd54c5f91a9a0ca584f7e8ec4b185c7f2971a4b0
Lucía Calzado <Luca Calzado@DESKTOP-TVMBU9D>
2019-12-26 20:30:04 +0100

    Final commit 3.0
    Añadida clase TokenInvalidoException.java.

--- src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/TokenInvalidoException.java
+++ src/main/java/edu/uclm/esi/iso2/banco20193capas/exceptions/TokenInvalidoException.java
@@ -1,5 +1,11 @@
 package edu.uclm.esi.iso2.banco20193capas.exceptions;
 
+/**
+ * Clase TokenInvalidoException.
+ * 
+ * @author Macario
+ *
+ */
 public class TokenInvalidoException extends Exception {
 
 }
