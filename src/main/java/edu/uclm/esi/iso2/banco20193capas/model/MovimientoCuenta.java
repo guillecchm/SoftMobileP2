ddd8cbc4ab3656855f463ee8ea6fc6af2a84fbd2
Lucía Calzado <Luca Calzado@DESKTOP-TVMBU9D>
2019-12-26 19:03:02 +0100

    Clase MovimientoCuenta.java 2.0
    Arreglados errores en la clase MovimientoCuenta: he quitado los final
    que daban
    errores en los tests.

--- src/main/java/edu/uclm/esi/iso2/banco20193capas/model/MovimientoCuenta.java
+++ src/main/java/edu/uclm/esi/iso2/banco20193capas/model/MovimientoCuenta.java
@@ -9,55 +9,67 @@ import javax.persistence.ManyToOne;
 /**
  * Representa un movimiento en una cuenta bancaria
  * 
+ * @author Macario
  */
 @Entity
 public class MovimientoCuenta {
-	@Id @GeneratedValue(strategy = GenerationType.AUTO) 
-	private Long id;
-	@ManyToOne
-	private Cuenta cuenta;
-	
-	private double importe;
-	private String concepto;
-	
-	public MovimientoCuenta() {
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private Cuenta cuenta;
+    private double importe;
+    private String concepto;
 
-	public MovimientoCuenta(Cuenta cuenta, double importe, String concepto) {
-		this.importe = importe;
-		this.concepto = concepto;
-		this.cuenta = cuenta;
-	}
+    /**
+     * empty constructor.
+     */
+    public MovimientoCuenta() {
+    }
 
-	public Long getId() {
-		return id;
-	}
+    /**
+     * Constructor.
+     * 
+     * @param cuenta
+     * @param importe
+     * @param concepto
+     */
+    public MovimientoCuenta(final Cuenta cuenta, final double importe,
+            final String concepto) {
+        this.importe = importe;
+        this.concepto = concepto;
+        this.cuenta = cuenta;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public Cuenta getCuenta() {
-		return cuenta;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setCuenta(Cuenta cuenta) {
-		this.cuenta = cuenta;
-	}
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
 
-	public double getImporte() {
-		return importe;
-	}
+    public void setCuenta(final Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
 
-	public void setImporte(double importe) {
-		this.importe = importe;
-	}
+    public double getImporte() {
+        return importe;
+    }
 
-	public String getConcepto() {
-		return concepto;
-	}
+    public void setImporte(final double importe) {
+        this.importe = importe;
+    }
 
-	public void setConcepto(String concepto) {
-		this.concepto = concepto;
-	}
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(final String concepto) {
+        this.concepto = concepto;
+    }
 }
